391552d7aa36a2c3b02ae13f7d40e62d946ff5da
swaridebbarma <98303710+swaridebbarma@users.noreply.github.com>
2022-11-15 10:55:06 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,16 @@
+var account = 3;
+
+switch (account) {
+    case 1:
+        console.log("Checking account");   // => Checking account
+        break;
+    case 2:
+        console.log("Savings account");    // => Savings account 
+        break;
+    case 3:
+        console.log("Business account");   // => Business account 
+        break;
+    default:
+        console.log("Invalid account code");
+        break;
+}
